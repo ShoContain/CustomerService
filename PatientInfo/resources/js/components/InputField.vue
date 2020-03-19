4c40e8dc626a02645bf8d2b1b7e46ab7050100fc
ShoContain <perblue.666@gmail.com>
2020-03-20 04:09:23 +0900

    初投稿

---
+++ PatientInfo/resources/js/components/InputField.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="relative">
+    <label :for="name" class="text-blue-500 text-sm form-bold absolute pt-1 pl-1">{{label}}</label>
+    <input type="text" :id="name" :placeholder="placeholder" @input="inputField()"
+     v-model="value" :class="errorClassObject()" class="pt-8 w-full border-b focus:outline-none focus:bg-gray-200 foucs:border-blue-400 pl-2 pb-2 text-blue-600">
+    <!-- <span v-text="msg"></span>　この２つは同じ　<span>{{msg}}</span> -->
+    <p class="text-red-500 text-sm" v-text="errorsMessage()"></p>
+</div>
+</template>
+
+<script>
+export default {
+
+    props: ['name', 'label', 'placeholder', 'errors'],
+    data: function() {
+        return {
+            value: '',
+        }
+    },
+    computed:{
+      hasError(){
+        return this.errors && this.errors[this.name] && this.errors[this.name].length > 0;
+      }
+    },
+    methods: {
+        inputField() {
+            this.errorClean();
+            this.$emit('update:field', this.value);
+        },
+        errorsMessage() {
+            if (this.hasError) {
+                return this.errors[this.name][0];
+            }
+        },
+        errorClean() {
+            if (this.hasError) {
+                this.errors[this.name] = null;
+            }
+        },
+        errorClassObject(){
+          return {
+            // 'class名'：class名を出力する条件式
+            'error-field':this.hasError,
+          }
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.error-field{
+  @apply .border-red-500 .border-b-2
+}
+</style>
